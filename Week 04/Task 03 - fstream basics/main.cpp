de43793b137c72ba593e39d5550e4fff55cbe97a
dovolniyzavodchanin@yandex.ru <dovolniyzavodchanin@yandex.ru>
2021-09-23 11:32:53 +0300

    fix: Task 03 (2/2) added write to file

--- Week 04/Task 03 - fstream basics/main.cpp
+++ Week 04/Task 03 - fstream basics/main.cpp
@@ -7,9 +7,11 @@ using namespace std;
 int main() {
 
 	ifstream input("input.txt");
+	ofstream output("output.txt");
 	string line;
 	while (getline(input, line)) {
 		cout << line << endl;
+		output << line << endl;
 	}
 
 	return 0;

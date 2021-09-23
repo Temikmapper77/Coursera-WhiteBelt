5b00a394b18f12820af9ad1525e4d1dc3ec27fbf
dovolniyzavodchanin@yandex.ru <dovolniyzavodchanin@yandex.ru>
2021-09-23 11:28:00 +0300

    init: Task 03 (1/2) done reading from file

---
+++ Week 04/Task 03 - fstream basics/main.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	ifstream input("input.txt");
+	string line;
+	while (getline(input, line)) {
+		cout << line << endl;
+	}
+
+	return 0;
+}
